8548e838341af423605a51cbd3826257b8e9ef93
Anuj srivastava <anuj7rivastava@gmail.com>
2025-08-09 21:58:23 +0530

    file added

--- customer/src/api/customer.js
+++ customer/src/api/customer.js
@@ -64,6 +64,6 @@ module.exports = (app) => {
     });
 
     app.get('/whoami', (req,res,next) => {
-        return res.status(200).json({msg: '/customer : I am Customer Service'})
+        return res.status(200).json({msg: '/customer : I am Calling from Customer Service'})
     })
 }
